7087bfb9dc3efcea19b89f3110fe32b0fee182e7
martinarvidsson <martinarvidsson1994@gmail.com>
2014-11-22 15:17:58 +0000

    klar med 1.1 fick ut meddelandet efter lite strul. glömde skriva window.OnLoad

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,1 +1,11 @@
 "use strict";
+Function Message (message, date){
+    
+    this.getText = function(){
+        return message;
+        }
+    this.setText = function(_text){
+        message = _text;
+        }
+    this.
+}
--- 2-labbymezzage/messageApp.js
+++ 2-labbymezzage/messageApp.js
@@ -1,1 +1,8 @@
 "use strict";
+var messageApp = {
+    init:function()
+    {
+        alert("I am an alert box!");
+    }
+}
+window.onload = messageApp.init
