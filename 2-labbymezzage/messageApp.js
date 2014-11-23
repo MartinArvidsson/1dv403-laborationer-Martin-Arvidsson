bc195c418ad037573c36e00b8f56ada761bdeb8b
martinarvidsson <martinarvidsson1994@gmail.com>
2014-11-23 18:49:38 +0000

    done with some css, moving on

--- 2-labbymezzage/messageApp.js
+++ 2-labbymezzage/messageApp.js
@@ -1,14 +1,14 @@
 "use strict";
 var messageApp = {
-    messages:[],-
+    messages:[],
     messagesSent: 0,
     init:function()
     {
-        var mess = new Message("Test1",new Date());
-        alert (mess);
-        alert (mess.getText());
-        mess.setText("Text2");
-        alert(mess);
+        //var mess = new Message("Test1",new Date());
+        //alert (mess);
+        //alert (mess.getText());
+       // mess.setText("Text2");
+       // alert(mess);
     }
 }
 window.onload = messageApp.init;
