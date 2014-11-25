e65e1b70b7a556d61fecaf4f7180d812fbc4fcc0
martinarvidsson <martinarvidsson1994@gmail.com>
2014-11-25 07:37:17 +0000

    small fixes

--- 2-labbymezzage/message.js
+++ 2-labbymezzage/message.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function Message (message, date){
+function Message (message, date){   //Skapar en funktion (Message) som innehåller message(meddelande) och date(datum)
     
     this.getText = function(){
         return message;
@@ -20,10 +20,10 @@ function Message (message, date){
         Message.prototype.toString = function(){
             return this.getText()+" ("+this.getDate()+")";
         };
-        Message.prototype.getHTMLText = function(){
+        Message.prototype.getHTMLText = function(){ //Byter användarens entertryck mot radbrytningar i presentationen
             return this.getText().replace(/[\n\r]/g, "<br/>");
         };
-        Message.prototype.getDateText = function (){
+        Message.prototype.getDateText = function (){ //Beräkning för nuvarande tid 
         var Hours = this.getDate().getHours();
         var Minutes = this.getDate().getMinutes();
         var Seconds = this.getDate().getSeconds();
--- 2-labbymezzage/messageApp.js
+++ 2-labbymezzage/messageApp.js
@@ -7,50 +7,52 @@ var messageApp = {
         var sendamessage = document.getElementById("sendmessage");
         sendamessage.onclick = function(e){
             if (document.getElementById("textarea").value !== "")
-            {
-                messageApp.sendmessage();
-            }
-            else
-            {
-                e.preventDefault();
-            }
+                {
+                    messageApp.sendmessage();
+                }
+                else
+                {
+                    e.preventDefault();
+                }
         };
         var sendbyEnter = document.getElementById("textarea");
         sendbyEnter.onkeypress=function(e)
         {
             if (e.keyCode ==13 && !e.shiftKey && document.getElementById("textarea").value !== "")
-            {
-                e.preventDefault();
-                messageApp.sendmessage();
-            }
+                {
+                    e.preventDefault();
+                    messageApp.sendmessage();
+                }
             if (e.keyCode == 13 && document.getElementById("textarea").value ==="")
-            {
-                e.preventDefault();
-            }
+                {
+                    e.preventDefault();
+                }
             };
         },
     
     sendmessage: function(){
-        var userInput = document.getElementById("textarea").value;
-        messageApp.messages.push(new Message(userInput, new Date()));
-        messageApp.messagesSent++;
-        document.getElementById("messagesSent").innerHTML = "Antal skickade meddelanden är : " + messageApp.messagesSent;
-        document.getElementById('textarea').value ="";
-        var arraylength = messageApp.messages.length-1;
-        messageApp.RenderMessage(arraylength);
-    },
+            
+            var userInput = document.getElementById("textarea").value;
+            messageApp.messages.push(new Message(userInput, new Date()));
+            messageApp.messagesSent++;
+            document.getElementById("messagesSent").innerHTML = "Antal skickade meddelanden är : " + messageApp.messagesSent;
+            document.getElementById('textarea').value ="";
+            var arraylength = messageApp.messages.length-1;
+            messageApp.RenderMessage(arraylength);
+        },
     RenderMessage: function(input){
-        var image = document.createElement("img");
+        
+        var Deletesprite = document.createElement("img");
         var Timesprite = document.createElement("img");
         var Timestamp = document.createElement("div");
         Timestamp.id = "Timestamp";
         var entry = document.createElement("section");
-        entry.id = "Timeandbutton";
+        entry.id = "TimeandDeletebutton";
         
         Timestamp.innerHTML = messageApp.messages[input].getDateText();
         
-        image.classname="Deletesprite";
-        image.src = "Delete.png";
+        Deletesprite.classname="Deletesprite";
+        Deletesprite.src = "Delete.png";
         Timesprite.classname ="Timesprite";
         Timesprite.src ="Clock.png";
 
@@ -59,7 +61,7 @@ var messageApp = {
         Textmessage.innerHTML = messageApp.messages[input].getHTMLText();
         
         Textmessage.appendChild(entry);
-        entry.appendChild(image);
+        entry.appendChild(Deletesprite);
         entry.appendChild(Timesprite);
         entry.appendChild(Timestamp);
         messagesarea.appendChild(Textmessage);
@@ -67,20 +69,21 @@ var messageApp = {
         var element = document.getElementById("messagesarea");
         element.scrollTop = element.scrollHeight;
         
-        image.onclick = function(e)
-        {
-            if(confirm("Vill du radera detta?"))
+        Deletesprite.onclick = function(e)
             {
-                messageApp.messagesSent = messageApp.messagesSent -1;
-                //messageApp.messages.splice(input, 1);
-                Textmessage.parentNode.removeChild(Textmessage);
-                document.getElementById("messagesSent").innerHTML = "Antal skickade meddelanden är : "+messageApp.messagesSent;
-            }
-        };
+                if(confirm("Vill du radera detta?"))
+                    {
+                        messageApp.messagesSent = messageApp.messagesSent -1;
+                        //messageApp.messages.splice(input, 1); // splicar, Utan splice tars sista meddelandet bort, men arrayen har kvar samma storlek. om jag använder
+                        //Splice har jag en bugg som jag inte vet hur jag löser.
+                        Textmessage.parentNode.removeChild(Textmessage);
+                        document.getElementById("messagesSent").innerHTML = "Antal skickade meddelanden är : "+messageApp.messagesSent;
+                    }
+            };
         Timesprite.onclick = function(e)
-        {
-            alert(messageApp.messages[input].getDate());
-        };
+            {
+                alert(messageApp.messages[input].getDate());
+            };
     }
 };
 window.onload = messageApp.init;
