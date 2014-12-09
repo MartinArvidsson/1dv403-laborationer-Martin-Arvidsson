7611d976e3f223715615754f2e738e211f412b36
martinarvidsson <martinarvidsson1994@gmail.com>
2014-12-09 09:26:58 +0000

    starting to work again, forgot to push earlier

---
+++ 4-thequiz/Questions.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var Quiz ={
+    tries: 0,
+    array:[],
+    init:function(){
+        Quiz.renderQuestion("http://vhost3.lnu.se:20080/question/1")
+    },
+    
+    renderQuestion:function(url){
+        Quiz.tries = 0;
+        var Serverquestion = document.getElementById("Question");
+        var XHR = new XMLHttpRequest();
+        var userInput = document.getElementById("Svar");
+        
+        userInput.value = "";
+        
+        XHR.onreadystatechange = function(){
+            if (XHR.readyState === 4 && XHR.status === 200)
+            {
+                var Serverobject = JSON.parse(XHR.responseText);
+                Serverquestion.innerHTML = Serverobject.question;
+                
+                document.getElementById("Button").addEventListener("click", function(e){
+                    if (userInput.value !== "")
+                    {
+                        //SVARA FRÅGA ( EJ KLAR)
+                    }
+                    else
+                    {
+                        e.preventDefault();
+                    }
+                })
+                document.getElementById("Svar").addEventListener("keydown", function(e) {
+                    if (e.keyCode === 13)
+                    {
+                        if (userInput.value !== "")
+                        {
+                            //SVARA
+                        }
+                        else
+                        {
+                            e.preventDefault();
+                        }
+                    }
+                })
+            }
+        }
+        XHR.open("GET",url , true)
+        XHR.send(null)
+    
+        
+    },
+    
+}
